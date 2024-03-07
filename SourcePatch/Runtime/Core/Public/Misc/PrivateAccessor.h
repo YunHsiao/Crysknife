f1ba0b94b136e3cd1f24807de10bdbaa98ea4005
Yun Hsiao Wu <yunhsiaow@gmail.com>
2024-03-07 15:55:09 +0800

    const member functions & template types

--- SourcePatch/Runtime/Core/Public/Misc/PrivateAccessor.h
+++ SourcePatch/Runtime/Core/Public/Misc/PrivateAccessor.h
@@ -33,8 +33,8 @@ TRob<Tag, Ptr> TRob<Tag, Ptr>::Obj;
 	struct F##ClassTag##MemberName##PrivateAccessor { typedef MemberType* Type; }; \
 	template struct TRob<F##ClassTag##MemberName##PrivateAccessor, &Class::MemberName>
 
-#define DEFINE_PRIVATE_ACCESSOR_FUNCTION_EX(Class, ReturnType, MemberName, ClassTag, ...) \
-	struct F##ClassTag##MemberName##PrivateAccessor { typedef ReturnType(Class::*Type)(__VA_ARGS__); }; \
+#define DEFINE_PRIVATE_ACCESSOR_FUNCTION_EX(Class, ReturnType, MemberName, ClassTag, Const, ...) \
+	struct F##ClassTag##MemberName##PrivateAccessor { typedef ReturnType(Class::*Type)(__VA_ARGS__) Const; }; \
 	template struct TRob<F##ClassTag##MemberName##PrivateAccessor, &Class::MemberName>
 
 #define DEFINE_PRIVATE_ACCESSOR_STATIC_FUNCTION_EX(Class, ReturnType, MemberName, ClassTag, ...) \
@@ -47,8 +47,13 @@ TRob<Tag, Ptr> TRob<Tag, Ptr>::Obj;
 
 #define DEFINE_PRIVATE_ACCESSOR(Class, MemberType, MemberName) DEFINE_PRIVATE_ACCESSOR_EX(Class, MemberType, MemberName, Class)
 #define DEFINE_PRIVATE_ACCESSOR_STATIC(Class, MemberType, MemberName) DEFINE_PRIVATE_ACCESSOR_STATIC_EX(Class, MemberType, MemberName, Class)
-#define DEFINE_PRIVATE_ACCESSOR_FUNCTION(Class, ReturnType, MemberName, ...) DEFINE_PRIVATE_ACCESSOR_FUNCTION_EX(Class, ReturnType, MemberName, Class, __VA_ARGS__)
+#define DEFINE_PRIVATE_ACCESSOR_FUNCTION(Class, ReturnType, MemberName, ...) DEFINE_PRIVATE_ACCESSOR_FUNCTION_EX(Class, ReturnType, MemberName, Class, , __VA_ARGS__)
+#define DEFINE_PRIVATE_ACCESSOR_CONST_FUNCTION(Class, ReturnType, MemberName, ...) DEFINE_PRIVATE_ACCESSOR_FUNCTION_EX(Class, ReturnType, MemberName, Class, const, __VA_ARGS__)
 #define DEFINE_PRIVATE_ACCESSOR_STATIC_FUNCTION(Class, ReturnType, MemberName, ...) DEFINE_PRIVATE_ACCESSOR_STATIC_FUNCTION_EX(Class, ReturnType, MemberName, Class, __VA_ARGS__)
+
+#define DEFINE_PRIVATE_ACCESSOR_FUNCTION_TAG(Class, ReturnType, MemberName, ClassTag, ...) DEFINE_PRIVATE_ACCESSOR_FUNCTION_EX(Class, ReturnType, MemberName, ClassTag, , __VA_ARGS__)
+#define DEFINE_PRIVATE_ACCESSOR_CONST_FUNCTION_TAG(Class, ReturnType, MemberName, ClassTag, ...) DEFINE_PRIVATE_ACCESSOR_FUNCTION_EX(Class, ReturnType, MemberName, ClassTag, const, __VA_ARGS__)
+#define DEFINE_PRIVATE_ACCESSOR_STATIC_FUNCTION_TAG(Class, ReturnType, MemberName, ClassTag, ...) DEFINE_PRIVATE_ACCESSOR_STATIC_FUNCTION_EX(Class, ReturnType, MemberName, ClassTag, __VA_ARGS__)
 
 #define PRIVATE_ACCESS_OBJ(ClassTag, MemberName, Obj) (Obj.*PRIVATE_ACCESS(ClassTag, MemberName))
 #define PRIVATE_ACCESS_PTR(ClassTag, MemberName, Ptr) (Ptr->*PRIVATE_ACCESS(ClassTag, MemberName))
@@ -62,6 +67,7 @@ TRob<Tag, Ptr> TRob<Tag, Ptr>::Obj;
 
 #include <cstdint>
 #include <cstdio>
+#include <map>
 
 class FTestClass
 {
@@ -70,8 +76,10 @@ class FTestClass
 	void Increment() { Value++; }
 	static bool Register(const FTestClass* Ptr) { Instance = Ptr; return true; }
 
+	void Register(std::map<const FTestClass*, int32_t>& Dictionary) const { Dictionary[this] = Value; }
+
 public:
-	void Print() const { printf("Instance %p Value %d\n", Instance, Value); fflush(stdout); }
+	void Print() const { printf("Instance %p Value %d\n", Instance, Value); }
 };
 
 inline const FTestClass* FTestClass::Instance = nullptr;
@@ -82,6 +90,9 @@ DEFINE_PRIVATE_ACCESSOR(FTestClass, int32_t, Value);
 DEFINE_PRIVATE_ACCESSOR_STATIC(FTestClass, const FTestClass*, Instance);
 DEFINE_PRIVATE_ACCESSOR_FUNCTION(FTestClass, void, Increment);
 DEFINE_PRIVATE_ACCESSOR_STATIC_FUNCTION(FTestClass, bool, Register, const FTestClass*);
+
+using FTestClassIndexMap = std::map<const FTestClass*, int32_t>; // Alias complex type names so we can pass to the macros
+DEFINE_PRIVATE_ACCESSOR_CONST_FUNCTION_TAG(FTestClass, void, Register, FTestClassToMap, FTestClassIndexMap&); // Use different tags for overloaded functions
 
 // Use it anywhere!
 
@@ -101,9 +112,15 @@ inline void PrivateAccessorTest()
 	bool bSuccess = PRIVATE_ACCESS_STATIC(FTestClass, Register)(Ptr);
 
 	// Set static variable
-	PRIVATE_ACCESS_STATIC(FTestClass, Instance) = nullptr;
+	PRIVATE_ACCESS_STATIC(FTestClass, Instance) = (const FTestClass*)0xdeadbeef;
+
+	// Invoke overloaded function
+	FTestClassIndexMap TestClassIndexMap;
+	PRIVATE_ACCESS_PTR(FTestClassToMap, Register, Ptr)(TestClassIndexMap);
 
 	Obj.Print();
+	printf("LocalValue %d Success %d MapValue %d\n", *Value, bSuccess, TestClassIndexMap[Ptr]);
+	fflush(stdout);
 }
 
 #endif
